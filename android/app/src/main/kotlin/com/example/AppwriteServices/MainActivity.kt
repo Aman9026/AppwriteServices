c4ee3fe637ed15ac2357ab3c8396ebe7f6c2dc01
Aman9026 <amangupta884036@gmail.com>
2020-10-02 19:10:58 +0530

    created basic app layout with drawer

---
+++ android/app/src/main/kotlin/com/example/AppwriteServices/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.AppwriteServices
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:foldable_sidebar/foldable_sidebar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Appwrite Services',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FSBStatus drawerStatus;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: FoldableSidebarBuilder(
+          drawerBackgroundColor: Colors.pink[100],
+          drawer: CustomDrawer(
+            closeDrawer: () {
+              setState(() {
+                drawerStatus = FSBStatus.FSB_CLOSE;
+              });
+            },
+          ),
+          screenContents: FirstScreen(),
+          status: drawerStatus,
+        ),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.pink,
+            child: Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              setState(() {
+                drawerStatus = drawerStatus == FSBStatus.FSB_OPEN
+                    ? FSBStatus.FSB_CLOSE
+                    : FSBStatus.FSB_OPEN;
+              });
+            }),
+      ),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink[100],
+      child: Center(
+        child: Text(
+          "Click on FAB to Open Drawer",
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomDrawer extends StatelessWidget {
+  final Function closeDrawer;
+
+  const CustomDrawer({Key key, this.closeDrawer}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    return Container(
+      color: Colors.white,
+      width: mediaQuery.size.width * 0.60,
+      height: mediaQuery.size.height,
+      child: Column(
+        children: <Widget>[
+          Container(
+              width: double.infinity,
+              height: 200,
+              color: Colors.grey.withAlpha(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'assets/appwritelogo.png',
+                    ),
+                    radius: 50,
+                  ),
+                  Padding(padding: const EdgeInsets.only(bottom: 30)),
+                  // SizedBox(
+                  //   height: 20,
+                  // ),
+                  Text(
+                    "Appwrite Services",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              )),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped Profile");
+            },
+            leading: Icon(Icons.person),
+            title: Text(
+              "Your Profile",
+            ),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.grey,
+          ),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped settings");
+            },
+            leading: Icon(Icons.settings),
+            title: Text("Settings"),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.grey,
+          ),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped Payments");
+            },
+            leading: Icon(Icons.payment),
+            title: Text("Payments"),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.grey,
+          ),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped Notifications");
+            },
+            leading: Icon(Icons.notifications),
+            title: Text("Notifications"),
+          ),
+          Divider(
+            height: 1,
+            color: Colors.grey,
+          ),
+          ListTile(
+            onTap: () {
+              debugPrint("Tapped Log Out");
+            },
+            leading: Icon(Icons.exit_to_app),
+            title: Text("Log Out"),
+          ),
+        ],
+      ),
+    );
+  }
+}
